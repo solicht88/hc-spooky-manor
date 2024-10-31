a215940df8e5fc96ab33a032f0d65de99c4bc44c
solicht88 <sakanaasai@gmail.com>
2024-10-31 18:27:18 -0400

    added buttons, images, favicon

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function closeWarning() {
     document.getElementById("warn-popup").style.display = "none";
+    document.getElementById("desktop").style.display = "grid";
 }
 
+function openWindow() {
+    document.getElementById("large-popup").style.display = "grid";
+}
